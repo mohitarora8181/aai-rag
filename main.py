b2eac7e698435241c5a39c8d60f1e8e9e05995ff
Rakesh Arora <mannyisha808@gmail.com>
2025-04-01 21:39:38 +0530

    fix 0.19

--- main.py
+++ main.py
@@ -99,7 +99,6 @@ def extract_page_content(pdf_path):
 
             img_masked = cv2.bitwise_and(img_np, img_np, mask=mask)            
             if img_masked is not None:
-                st.image(img_masked)
                 ocr_results = ocr_reader.readtext(img_masked, paragraph=True)
                 
             text_blocks = []
